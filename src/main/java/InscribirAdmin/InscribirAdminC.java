849fed0923ea4dad0491f2e954c2b5b8286dc7a1
UO276268 <UO276268@GI-AN-B7_12.aulasuo.uniovi.es>
2023-03-22 12:42:05 +0100

    Clase InscribirAdmin

---
+++ src/main/java/InscribirAdmin/InscribirAdminC.java
@@ -0,0 +1,5 @@
+package InscribirAdmin;
+
+public class InscribirAdminC {
+
+}
---
+++ src/main/java/InscribirAdmin/InscribirAdminV.java
@@ -0,0 +1,43 @@
+package InscribirAdmin;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+public class InscribirAdminV {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					InscribirAdminV window = new InscribirAdminV();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public InscribirAdminV() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
