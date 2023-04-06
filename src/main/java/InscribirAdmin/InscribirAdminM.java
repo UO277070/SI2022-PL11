de3a39939ece7eda0a5112987ff1cb2ab0f0677c
martinbp3 <uo276268@uniovi.es>
2023-04-06 19:03:04 +0200

    Consultas

--- src/main/java/InscribirAdmin/InscribirAdminM.java
+++ src/main/java/InscribirAdmin/InscribirAdminM.java
@@ -8,12 +8,36 @@ import giis.demo.util.Database;
 public class InscribirAdminM {
 	private Database db = new Database();
 	
-	public List<Object[]> getListaActividades(String periodo){
+	public List<Object[]> getListaActividadesEnPeriodoSocio(String fechaActual){
 		String sql = "SELECT Actividad.nombre FROM Actividad "
 				+ "INNER JOIN PeriodoInscripcion "
 				+ "ON Actividad.idPeriodoinscrip = PeriodoInscripcion.idPeriodoinscrip "
-				+ "WHERE PeriodoInscripcion.nombre=?";
-		return db.executeQueryArray(sql, periodo);
+				+ "WHERE PeriodoInscripcion.fechainscripini <= ? AND PeriodoInscripcion.fechainscripfin >= ? ";
+		return db.executeQueryArray(sql, fechaActual);
+	}
+	
+	public List<Object[]> getListaActividadesEnPeriodoInsNoSocio(String fechaActual){
+		String sql = "SELECT Actividad.nombre FROM Actividad "
+				+ "INNER JOIN PeriodoInscripcion "
+				+ "ON Actividad.idPeriodoinscrip = PeriodoInscripcion.idPeriodoinscrip "
+				+ "WHERE PeriodoInscripcion.fechainscripini <= ? AND PeriodoInscripcion.fechainscripfinnosocio >= ? ";
+		return db.executeQueryArray(sql, fechaActual);
+	}
+	
+	public List<Object[]> getNumInscripcionesEnActividad(int idActividad) {
+		String sql = "SELECT count(*) FROM inscripcion WHERE idActividad=?;";
+		return db.executeQueryArray(sql, idActividad);
+	} 
+	
+	public List<Object[]> getInscripcionesEnActividad(int idActividad){
+		String sql = "SELECT idActividad, idSocio, idNosocio FROM Inscripcion"
+				+ "WHERE idActividad = ?";
+		return db.executeQueryArray(sql, idActividad);
+	}
+	
+	public List<Object[]> getPlazasTotalesActividad(int idActividad){
+		String sql = "SELECT plazas FROM Actividad WHERE idActividad = ?";
+		return db.executeQueryArray(sql, idActividad);
 	}
 	
 	public List<Object[]> getListaSocios(){
